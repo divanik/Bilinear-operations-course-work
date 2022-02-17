457ea02488831fbe8c1bbfc3c8864d1430bf7c86
Daniil Ivanik <ivanik01@yandex.ru>
2022-02-17 15:51:09 +0300

    Strange bugs

---
+++ src/ttrandombilinear/contraction.py
@@ -0,0 +1,29 @@
+import typing
+import numpy as np
+
+def partialContractionsRL(tt_tensors1 : typing.List[np.array], tt_tensors2 : typing.List[np.array]):
+    answer = []
+    last = np.ones((1, 1))
+    #print(tt_tensors1[0].shape)
+    #print(tt_tensors1[1].shape)
+    #print(tt_tensors1[2].shape)
+    #print(tt_tensors2[0].shape)
+    #print(tt_tensors2[1].shape)
+    #print(tt_tensors2[2].shape)
+    for idx, tt1, tt2 in (zip(reversed(range(len(tt_tensors1))), reversed(tt_tensors1), reversed(tt_tensors2))):
+        if idx > 0:
+            #print(idx)
+            last = np.einsum('ijk,kl,mjl->im', tt1, last, tt2)
+            answer.append(last)
+    return list(reversed(answer))
+
+def partialContractionsLR(tt_tensors1 : typing.List[np.array], tt_tensors2 : typing.List[np.array]):
+    answer = []
+    cur = np.ones((1, 1))
+    for idx, tt1, tt2 in zip(range(len(tt_tensors1)), tt_tensors1, tt_tensors2):
+        if idx < (len(tt_tensors1) - 1):
+            cur = np.einsum('ijk,il,ljm->km', tt1, cur, tt2)
+            answer.append(cur)
+    return answer
+
+#tt1 = [np.array([[3.0, 4.0, ]])]
---
+++ tests/test_bilinear_operations.py
@@ -0,0 +1,21 @@
+import numpy as np
+import src.ttrandombilinear.bilinear_operations as bilinear_operations
+import src.ttrandombilinear.primitives as primitives
+
+t0 = np.array([ [[4 , 5, 6], [-4, 7, 8], [1, -4, 5]] ])
+
+t1 = np.array([[[2, 3, 4], [5, 2, 8]], [[1, -4, 1], [1, -2, 0]], [[6, -4, -3], [4, 2, -7]]])
+
+t2 = np.array([ [[-2] ,[9], [-1]], [[9], [-10], [1]], [[1], [4], [2]] ])
+
+tt = [t0, t1, t2]
+
+print(t0.shape)
+print(t1.shape)
+print(t2.shape)
+
+sum0 = primitives.countTensor(bilinear_operations.roundingSumRandThenOrth(tt, tt, [2, 2]))
+sum1 = primitives.countTensor(tt) + primitives.countTensor(tt)
+
+print( primitives.countTensor(tt) )
+print( primitives.frob(primitives.frob(sum0) - primitives.frob(sum1)) / np.sqrt(primitives.frob(sum0) * primitives.frob(sum1)) )
